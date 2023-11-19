8dc14bc195abb81513dc013284a1a7f559287bd2
maodemo <1933056636@qq.com>
2023-11-19 22:42:02 +0800

    008

--- python/008.py
+++ python/008.py
@@ -5,10 +5,16 @@ class Solution:
         i = 0
         nums = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
         for i in range(len(s)):
-            if s[i] in nums:     
-                sint = sint + s[i]
+            if s[i] == ' ':
+                continue
+            elif s[i] == '+':
+                continue
             elif s[i] == '-':
                 intnum = -1
+            elif s[i] in nums:     
+                sint = sint + s[i]
+            else:
+                break
         return intnum * int(sint)
     
 x = "words and 987"
