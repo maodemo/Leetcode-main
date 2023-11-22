482e31e6e32e4feb599b6336515f67612345ad27
maodemo <1933056636@qq.com>
2023-11-22 20:38:50 +0800

    008

--- python/008.py
+++ python/008.py
@@ -1,22 +1,39 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        sint = ''
-        intnum : int = 1
-        i = 0
-        nums = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-        for i in range(len(s)):
-            if s[i] == ' ':
-                continue
-            elif s[i] == '+':
-                continue
-            elif s[i] == '-':
-                intnum = -1
-            elif s[i] in nums:     
-                sint = sint + s[i]
+        num: int = 1
+        numstr = ''
+        numlist = set(str(i) for i in range(10))
+        numlist.update({' ', '-', '+'})
+        for i in s:
+            if i in numlist:
+                if i == ' ':
+                    continue
+                elif i == '+':
+                    numlist = set(str(i) for i in range(10))
+                elif i == '-':
+                    numlist = set(str(i) for i in range(10))
+                    num = -1
+                else:
+                    numlist = set(str(i) for i in range(10))
+                    numstr = numstr + i
             else:
                 break
-        return intnum * int(sint)
+        if numstr == '':
+            numstr = 0
+        num = num * int(numstr)
+        return self.RegionNum(num)
+
+
+    def RegionNum(self, num) -> int:
+        if num < - (2 ** 31):
+            return( - (2 ** 31))
+        elif num > 2 ** 31 - 1:
+            return(2 ** 31 - 1)
+        else:
+            return(num)
+        
+
     
-x = "words and 987"
+x = "00000-42a1234"
 result = Solution()
 print(result.myAtoi(x))
